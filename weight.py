14d66f902afe0a8f439c7051931dcd3bc9f941bc
Purity Masanya <masanyapurity@gmail.com>
2025-03-20 16:14:11 +0300

    Initial commit

---
+++ weight.py
@@ -0,0 +1,13 @@
+weight= float(input("Enter your weight: "))
+unit= input("Kilograms or Pounds? (K or L): ").strip().upper()
+
+if unit == "K":
+    weight = weight * 2.205
+    print(f"Your weight is: {round(weight, 1)} Lbs.")
+
+elif unit == "L":
+    weight = weight/2.205
+    print(f"Your weight is: {round(weight,1)} Kgs.") 
+
+else:
+    print(f"{unit} is not valid.Please enter 'K' for Kilograms or 'L' for Pounds.")    
